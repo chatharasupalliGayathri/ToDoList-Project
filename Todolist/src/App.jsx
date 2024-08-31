91331180da725f54c969b2e07f4919e87a0b43e7
Gayathri <gay3ch567@gmail.com>
2024-08-31 23:16:39 +0530

    first commit

---
+++ Todolist/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import Home from './Home'
+import './App.css'
+import Create from './Create'
+function App() {
+ 
+
+  return (
+    
+    <div>
+      <Home />
+      </div>
+  )
+}
+
+export default App
